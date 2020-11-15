1672f9b9cf9dbf3d0654a4820968960a6ac13a0c
MagicRomero <magicromero@protonmail.com>
2020-11-15 16:05:21 +0000

    Ampliada la configuracion para manejar imagenes y fuentes (incluso importandolas de url externas)

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = {
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    chunkFilename: "[id].[chunkhash].js",
+  },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     port: 8080,
     compress: true,
+  },
+  optimization: {
+    splitChunks: { chunks: "all" },
   },
   watch: true,
   watchOptions: {
@@ -13,6 +22,9 @@ module.exports = {
       path.resolve(__dirname, "dist"),
       path.resolve(__dirname, "node_modules"),
     ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
   module: {
     rules: [
@@ -34,16 +46,52 @@ module.exports = {
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
     ],
   },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
   plugins: [
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        //Lossless
+        plugins: [
+          ["gifsicle", { interlaced: true }],
+          ["jpegtran", { progressive: true }],
+          ["optipng", { optimizationLevel: 5 }],
+          [
+            "svgo",
+            {
+              plugins: [
+                {
+                  removeViewBox: false,
+                },
+              ],
+            },
+          ],
+        ],
+      },
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src/public", "index.html"),
     }),
